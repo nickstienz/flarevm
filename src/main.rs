b43ad1d637863cc44ff371dd0d19e05e5c1ac4d8
Nicholas Stienz <nickstienz@gmail.com>
2023-10-13 22:01:26 -0500

    README update

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,7 @@ mod cli;
 use cli::*;
 
 fn main() {
-    /*
-    The FVM will need to handle some args so they are handled first.
-    They are passed in to be pasrsed and then a Cli is returned with the
-    right settings. Of course, this functionality needs to be
-    implemented first :D Have fun!
-    */
     let args: Vec<String> = std::env::args().collect();
     let cli = Cli::handle_args(&args);
-
-    println!("{:?}", cli);
+    println!("{:?}", cli); // TODO: Let's work on other stuff first
 }
