b7f465d00d86a7bce546a26155e4b357cadbb8f5
Nicholas Stienz <nickstienz@gmail.com>
2023-12-03 15:00:10 -0600

    Added unit tests to hex utils

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ mod utils;
 fn main() {
     // This is the gate to hell. I wish you luck traveler.
     let program: Vec<u8> = vec![
+        /* This prints "Hello, World!\n"
+        pushs 15, "Hello, World!\n"
+        print
+        exit 0
+        */
         0x04, 0x0E, 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2C, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x21,
         0x0A, 0x05, 0x00, 0x00,
     ];
--- src/utils/hex.rs
+++ src/utils/hex.rs
@@ -37,3 +37,50 @@ pub fn to_str(bytes: &[u8]) -> &str {
         Err(e) => Error::panic(Error::ByteToString, e.to_string()),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn to_int_too_small() {
+        let list: Vec<u8> = vec![0x01];
+        to_int::<u64>(&list);
+    }
+
+    #[test]
+    #[should_panic]
+    fn to_int_too_big() {
+        let list: Vec<u8> = vec![0x01, 0x01];
+        to_int::<u8>(&list);
+    }
+
+    #[test]
+    fn to_int_u32() {
+        let list: Vec<u8> = vec![0x00, 0x00, 0x00, 0x01];
+        let num = to_int::<u32>(&list);
+        assert_eq!(num, 1u32);
+    }
+
+    #[test]
+    fn to_str_a() {
+        let list: Vec<u8> = vec![0x61];
+        let s = to_str(&list);
+        assert_eq!(s, "a");
+    }
+
+    #[test]
+    fn to_str_abc() {
+        let list: Vec<u8> = vec![0x61, 0x62, 0x63];
+        let s = to_str(&list);
+        assert_eq!(s, "abc");
+    }
+
+    #[test]
+    #[should_panic]
+    fn to_str_invalid_utf8() {
+        let list: Vec<u8> = vec![0xFC, 0x53];
+        to_str(&list);
+    }
+}
