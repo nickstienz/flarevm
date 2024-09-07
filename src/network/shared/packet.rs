99068360431d4af68dbdc55e659ed318f7ec669c
Nicholas Stienz <nickstienz@gmail.com>
2024-09-07 18:48:32 -0500

    Cleaning up packet

--- src/network/shared/packet.rs
+++ src/network/shared/packet.rs
@@ -10,7 +10,7 @@ pub type ChecksumType = u16;
 /// The packet is used to send data between the server and client to
 /// interact with the VM.
 ///
-/// > The packets will be sent using the TCP protocal.
+/// The packets will be sent using the TCP protocal.
 ///
 /// The header is 6 bytes long and after that is the data and checksum.
 pub struct Packet {
@@ -18,7 +18,6 @@ pub struct Packet {
     /// always be `0xAD01`.
     magic_number: u16,
     /// The version will increment on packet implementation changes.
-    ///
     /// This includes changes to the `PacketType` enum.
     version: u8,
     /// An enum that detemines how to handle a packet. This enum will be
@@ -39,15 +38,6 @@ impl Packet {
     /// The `new()` function returns a new packet based on the given
     /// `PacketType` and data (`[u8]`). It can then be converted to binary
     /// for sending over the internet.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// let p = Packet::new(PacketType::None, Vec::new());
-    /// let p_hex = p.get_hex();
-    ///
-    /// assert_eq!(p_hex, "AD0100FF020000");
-    /// ```
     pub fn new(p_type: PacketType, data: Vec<u8>) -> Self {
         Packet {
             magic_number: MAGIC_NUMBER,
@@ -58,15 +48,38 @@ impl Packet {
             checksum: 0, // TODO: Compute checksum based on data
         }
     }
+
+    /// The `get_bytes()` function will return a vector of `u8` that
+    /// represents the original packet.
+    ///
+    /// This will convert any none numerical data types into numerical ones
+    /// while organizing everything into the final structure.
+    pub fn get_bytes(&self) -> Vec<u8> {
+        let mut bytes: Vec<u8> = Vec::new();
+
+        bytes.extend_from_slice(&self.magic_number.to_be_bytes());
+        bytes.push(self.version);
+        bytes.push(self.p_type as u8);
+        bytes.extend_from_slice(&self.length.to_be_bytes());
+
+        if !self.data.is_empty() {
+            bytes.extend_from_slice(&self.data);
+        }
+
+        bytes.extend_from_slice(&self.checksum.to_be_bytes());
+        bytes
+    }
 }
 
 /// The `PacketType` is used to detemine what actions the VM must take
 /// to interpret the data being passed to it.
 ///
-/// All types are given a number used to identify it in the final packet.
-/// This number should not be changed unless necessary and if it is, the
-/// `VERSION` constant should be incremented.
+/// All types are given a number in the `u8` range `(0-255 or 0x00-0xFF)` used
+/// to identify it in the final packet. This number should not be changed
+/// unless necessary and if it is, the `VERSION` constant should be incremented
+/// to reflect that the change will break other versions.
 #[repr(u8)]
+#[derive(Clone, Copy)]
 pub enum PacketType {
     /// The `None` type is used for testing and will be ignored by the VM.
     None = 0xFF,
@@ -77,9 +90,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_empty_packet_as_hex() {
+    fn create_empty_packet_as_bytes() {
         let p = Packet::new(PacketType::None, Vec::new());
 
-        assert_eq!(p.get_hex(), "AD0100FF020000");
+        assert_eq!(
+            p.get_bytes(),
+            vec![0xAD, 0x01, 0x00, 0xFF, 0x00, 0x02, 0x00, 0x00]
+        );
     }
 }
