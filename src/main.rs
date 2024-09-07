5cc2a35baef0d751cd77b76c7cc5cd78028b3c16
Nicholas Stienz <nickstienz@gmail.com>
2024-09-06 19:50:31 -0500

    Update gitignore and main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Humble beginnings o_o");
+    cli_header();
+}
+
+fn cli_header() {
+    println!("================================================");
+    println!("=== This program was made by Nicholas Stienz ===");
+    println!("===  This software is under the MIT License  ===");
+    println!("===------------------------------------------===");
+    println!("===  !!! This program is alpha software !!!  ===");
+    println!("===  !!!       Expect it to break       !!!  ===");
+    println!("================================================");
+    println!();
 }
