7c4c5dcb1b5d1b01fa3afab7675767cd725e4289
Nicholas Stienz <nickstienz@gmail.com>
2024-09-07 22:52:16 -0500

    Made main look better and got rid of cringe header

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,17 @@
-use network::shared::packet::Packet;
+use network::shared::packet::{Packet, PacketType};
 
 pub mod network;
 
 fn main() {
-    cli_header();
-
-    let p = network::shared::packet::Packet::new(
-        network::shared::packet::PacketType::None,
+    let p = Packet::new(
+        PacketType::None,
         vec![0x01, 0x02, 0x03, 0x04, 0xA3, 0x17, 0xDC, 0x0A],
     );
 
-    println!("Packet: {:#?}", p);
-    println!("Packet encoded: {:?}", p.encode_packet());
+    println!("===Original Packet===\n{:#?}\n", p);
+    println!("Packet encoded: {:?}\n", p.encode_packet());
 
     // Decode
     let dec = Packet::decode_packet(&p.encode_packet());
-    println!("Packet decoded{:#?}", dec);
-}
-
-fn cli_header() {
-    println!("================================================");
-    println!("=== This program was made by Nicholas Stienz ===");
-    println!("===  This software is under the MIT License  ===");
-    println!("===------------------------------------------===");
-    println!("===  !!! This program is alpha software !!!  ===");
-    println!("===  !!!       Expect it to break       !!!  ===");
-    println!("================================================");
-    println!();
+    println!("===Packet Decoded===\n{:#?}", dec);
 }
