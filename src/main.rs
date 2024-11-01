b821735c8605854428fd3516ba897dbb3aa7db6e
Nicholas Stienz <nickstienz@gmail.com>
2024-11-01 13:38:28 -0500

    Make main look pretty again

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,14 @@ use network::shared::packet::{Packet, PacketType};
 pub mod network;
 
 fn main() {
+    // Encode
     let p = Packet::encode(
         PacketType::None,
         &[0x01, 0x02, 0x03, 0x04, 0xA3, 0x17, 0xDC, 0x0A],
     );
-
-    println!("===Original Packet===\n{:#?}\n", p);
-
-    // Encode
-    // println!("Packet encoded: {:?}\n", p.encode_packet());
+    println!("===Original Packet===\n{:?}\n", p);
 
     // Decode
-    // let dec = Packet::decode_packet(&p.encode_packet());
     let dec = Packet::decode_packet(&p);
-    println!("===Packet Decoded===\n{:#?}", dec);
+    println!("===Packet Decoded===\n{:?}", dec);
 }
