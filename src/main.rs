195fd66d80e5c746be00d640761b570dd45a9769
Nicholas Stienz <nickstienz@gmail.com>
2024-11-18 13:19:39 -0600

    Testing performance

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,19 @@ use network::shared::packet::{Packet, PacketType};
 pub mod network;
 
 fn main() {
-    // Encode
-    let p = Packet::encode(
-        PacketType::None,
-        &[0x01, 0x02, 0x03, 0x04, 0xA3, 0x17, 0xDC, 0x0A],
-    );
-    println!("===Original Packet===\n{:?}\n", p);
+    (0..1000000).for_each(|_| {
+        let _p = Packet::encode(PacketType::None, &[5]);
+        // println!("{:?}", p);
+    });
 
-    // Decode
-    let dec = Packet::decode_packet(&p);
-    println!("===Packet Decoded===\n{:?}", dec);
+    // Encode
+    // let p = Packet::encode(
+    //     PacketType::None,
+    //     &[0x01, 0x02, 0x03, 0x04, 0xA3, 0x17, 0xDC, 0x0A],
+    // );
+    // println!("===Original Packet===\n{:?}\n", p);
+    //
+    // // Decode
+    // let dec = Packet::decode_packet(&p);
+    // println!("===Packet Decoded===\n{:?}", dec);
 }
